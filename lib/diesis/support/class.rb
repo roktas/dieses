34452a7fa06f58ac2e76ab82bb64c9b79857042b
Recai Oktaş <roktas@gmail.com>
2021-08-04 23:41:23 +0300

    .

--- lib/diesis/support/class.rb
+++ lib/diesis/support/class.rb
@@ -28,8 +28,8 @@ module Diesis
         end.freeze
 
         class << self
-          def behave(behave, value = Const::Undefined)
-            Const::Undefined.equal?(behave) ? implicit(value) : explicit(behave)
+          def behave(behave, value = Undefined)
+            Undefined.equal?(behave) ? implicit(value) : explicit(behave)
           end
 
           private
@@ -58,15 +58,15 @@ module Diesis
       private_constant :Value
 
       # rubocop:disable Metrics/MethodLength,Layout/LineLength,Lint/RedundantCopDisableDirective
-      def define(name, default: Const::Undefined, behave: Const::Undefined, inherit: true, instance_reader: false)
+      def define(name, default: Undefined, behave: Undefined, inherit: true, instance_reader: false)
         ivar   = :"@#{name}"
         behave = Value.behave(behave, default)
 
         instance_variable_set(ivar, default.dup)
 
         mod = ::Module.new do
-          define_method(name) do |new_value = Const::Undefined|
-            if Const::Undefined.equal?(new_value)
+          define_method(name) do |new_value = Undefined|
+            if Undefined.equal?(new_value)
               return instance_variable_defined?(ivar) ? instance_variable_get(ivar) : nil
             end
 
@@ -92,7 +92,7 @@ module Diesis
       end
       # rubocop:enable Metrics/MethodLength,Layout/LineLength,Lint/RedundantCopDisableDirective
 
-      def defines(*names, behave: Const::Undefined)
+      def defines(*names, behave: Undefined)
         names.each { |name| define(name, behave: behave) }
       end
     end
