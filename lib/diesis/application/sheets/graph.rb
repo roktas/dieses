22e0f277ed3916c219a4c8a4cd997ccf09061882
Recai Oktaş <roktas@gmail.com>
2021-08-08 00:59:14 +0300

    .

--- lib/diesis/application/sheets/graph.rb
+++ lib/diesis/application/sheets/graph.rb
@@ -9,7 +9,7 @@ module Diesis
         include Mixins::Lines
         include Mixins::Squares
 
-        variate unit: [5, 7, 10], square: [8] do
+        variate unit: [5, 7, 10], square: [6, 8] do
           self.name = "#{unit}#{square}"
           self.desc = "#{square} squares with #{unit} mm unit"
         end
