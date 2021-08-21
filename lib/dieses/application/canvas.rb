772339fe25d01918138be123a2e89db10c0a110d
Recai Oktaş <roktas@gmail.com>
2021-08-21 19:54:03 +0300

    feat(scribes): Swap waist and base colors

--- lib/dieses/application/canvas.rb
+++ lib/dieses/application/canvas.rb
@@ -8,7 +8,7 @@ module Dieses
       TEMPLATE = <<~XML
         <?xml version="1.0" standalone="no"?>
         <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
-        <svg xmlns="http://www.w3.org/2000/svg" width="%{width}mm" height="%{height}mm" viewBox="0 0 %{width} %{height}" %{header}>
+        <svg xmlns="http://www.w3.org/2000/svg" width="%{width}mm" height="%{height}mm" viewBox="0 0 %{width} %{height}" shape-rendering="geometricPrecision" %{header}>
           <style>
             svg       { stroke: %{color}; stroke-width: %{medium}; }
             .altcolor { stroke: %{altcolor}; }
