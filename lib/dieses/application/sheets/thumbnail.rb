760ce2054a8cc0fc11269b88156ac7362a20c072
Recai Oktaş <roktas@gmail.com>
2021-08-25 00:14:04 +0300

    refactor: Reimplement thumbnails

--- lib/dieses/application/sheets/thumbnail.rb
+++ lib/dieses/application/sheets/thumbnail.rb
@@ -6,16 +6,20 @@ module Dieses
       class Thumbnail < Sheet
         sheet :thumbnail, 'Thumbnails'
 
+        style linewidth: 0.3, color: '#007f80'
+
         DENSITY = {
-          '1': Param.(row: 4, col: 4, width: 8, height: 10),
-          '3': Param.(row: 3, col: 6, width: 8, height: 10),
-          '2': Param.(row: 5, col: 5, width: 6, height: 8),
-          '4': Param.(row: 4, col: 7, width: 6, height: 8)
+          '1': Param.(row: 5, col: 3, width: 10, height: 10),
+          '2': Param.(row: 4, col: 4, width: 8,  height: 10),
+          '3': Param.(row: 5, col: 5, width: 6,  height: 8),
+          '4': Param.(row: 6, col: 5, width: 6,  height: 6)
         }.freeze
 
-        variate density: %i[1 2 3 4], unit: 5 do
-          self.name = density.to_s
-          self.desc = "Density #{density}"
+        variate density: DENSITY.keys, unit: 5 do
+          self.name = density
+
+          param = DENSITY[density]
+          self.desc = "Density #{density} (#{param.row}x#{param.col} thumbnails on A4 paper)"
         end
 
         def call
@@ -25,9 +29,9 @@ module Dieses
             repeat row do
               repeat col do
                 rect :rect, width: width, height: height
-                right(width + 1)
+                right(width + 1.25)
               end
-              down(height + 1)
+              down(height + 1.25)
             end
           end
         end
