19da6280057a95d7bc7a5094522dd7710a0d4e79
Recai Oktaş <roktas@gmail.com>
2021-08-26 01:37:06 +0300

    style: Minor improvements

--- lib/dieses/application/sheets/table.rb
+++ lib/dieses/application/sheets/table.rb
@@ -8,22 +8,22 @@ module Dieses
 
         style linewidth: 0.2, color: '#007f80'
 
-        DENSITY = {
+        TABLES = {
           '1': Param.(row: 2,  col: 2),
           '2': Param.(row: 5,  col: 2),
           '3': Param.(row: 4,  col: 4),
           '4': Param.(row: 10, col: 4)
         }.freeze
 
-        variate density: DENSITY.keys, unit: 1 do
-          self.name = density
+        variate table: TABLES.keys, unit: 1 do
+          self.name = table
 
-          param = DENSITY[density]
-          self.desc = "Density #{density} (#{param.row}x#{param.col} table on A4 paper)"
+          param = TABLES[table]
+          self.desc = "Table #{table} (#{param.row}x#{param.col} table on A4 paper)"
         end
 
         def call # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
-          row, col = DENSITY[param.density].to_a
+          row, col = TABLES[param.table].to_a
 
           cell_width,  total_width  = Ruler.divide(unit: param.unit, multiple: col, length: canvas.width)
           cell_height, total_height = Ruler.divide(unit: param.unit, multiple: row, length: canvas.height)
@@ -33,14 +33,14 @@ module Dieses
               hline :hline, length: total_width
               down cell_height
             end
-            repeat col + 1 do
-              vline :vline, length: total_height
-              right cell_width
-            end
             repeat row do
               down cell_height / 2
               hline :half, :dashed, :fine, length: total_width
               down cell_height / 2
+            end
+            repeat col + 1 do
+              vline :vline, length: total_height
+              right cell_width
             end
           end
         end
