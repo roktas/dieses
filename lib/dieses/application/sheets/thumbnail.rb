be30d2b27b0c73b1b8f4e88091f80054bdce5042
Recai Oktaş <roktas@gmail.com>
2021-08-26 01:42:36 +0300

    style: Minor improvements

--- lib/dieses/application/sheets/thumbnail.rb
+++ lib/dieses/application/sheets/thumbnail.rb
@@ -8,22 +8,22 @@ module Dieses
 
         style linewidth: 0.3, color: '#007f80'
 
-        DENSITY = {
+        DENSITIES = {
           '1': Param.(row: 5, col: 3, width: 10, height: 10),
           '2': Param.(row: 4, col: 4, width: 8,  height: 10),
           '3': Param.(row: 5, col: 5, width: 6,  height: 8),
           '4': Param.(row: 6, col: 5, width: 6,  height: 6)
         }.freeze
 
-        variate density: DENSITY.keys, unit: 5 do
+        variate density: DENSITIES.keys, unit: 5 do
           self.name = density
 
-          param = DENSITY[density]
+          param = DENSITIES[density]
           self.desc = "Density #{density} (#{param.row}x#{param.col} thumbnails on A4 paper)"
         end
 
         def call
-          row, col, width, height = DENSITY[param.density].to_a
+          row, col, width, height = DENSITIES[param.density].to_a
 
           draw unit: param.unit do
             repeat row do
